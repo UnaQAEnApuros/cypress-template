ab6f894f8b75589c16e456f9208f36e4fb7baf45
Estefanía Fdez Muñoz <estefafdez@gmail.com>
2020-12-28 18:44:33 +0100

    add extension

--- cypress/integration/web/web.spec.js
+++ cypress/integration/web/web.spec.js
@@ -2,7 +2,7 @@
 /// <reference types="Cypress" />
 const env = Cypress.env();
 import { qase } from 'cypress-qase-reporter/dist/mocha';
-import HomePage from '../../pageobjects/HomePage';
+import HomePage from '../../pageobjects/HomePage.js';
 
 describe('Web Example', function () {
 
