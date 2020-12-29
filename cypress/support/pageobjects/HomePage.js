7e53b1ce7379f073018abdfc1424b77d24f7f433
Estefanía Fdez Muñoz <estefafdez@gmail.com>
2020-12-29 19:35:20 +0100

    add more methods

--- cypress/support/pageobjects/HomePage.js
+++ cypress/support/pageobjects/HomePage.js
@@ -1,6 +1,6 @@
 import Header from './Header';
 import Footer from './Footer';
-import SimpleFormDemoPage from './SimpleFormDemoPage';
+import SimpleFormDemoPage from './inputforms/SimpleFormDemoPage';
 
 const env = Cypress.env();
 
