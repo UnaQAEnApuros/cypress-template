54f613a63018e7cf8846ce7a717b64620c13adbd
Estefanía Fdez Muñoz <estefafdez@gmail.com>
2021-02-25 11:16:17 +0100

    remove duplicated

--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -38,13 +38,6 @@ Cypress.on('test:after:run', (test, runnable) => {
 	}
 });
 
-// To turn off all uncaught exception handling
-
-Cypress.on('uncaught:exception', (err, runnable) => {
-	// returning false here prevents Cypress from failing the test
-	return false
-});
-
 /**
  * Method to avoid a failure on the tests due to uncaught exceptions.
  * More info here: https://docs.cypress.io/guides/references/error-messages.html#Uncaught-exceptions-from-your-application
