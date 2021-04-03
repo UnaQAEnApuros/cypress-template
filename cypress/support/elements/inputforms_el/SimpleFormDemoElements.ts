ee8db9e6dc3b6c88f237f2845668a0479606a8b5
Estefanía Fdez Muñoz <estefafdez@gmail.com>
2021-04-03 19:13:38 +0200

    add 2 new tests

---
+++ cypress/support/elements/inputforms_el/SimpleFormDemoElements.ts
@@ -0,0 +1,34 @@
+class SimpleFormDemoElements {
+  /**
+   * Method to get the Cross Browser Testing Logo
+   */
+  getUserMessageInput() {
+    return cy.get('.form-group > #user-message')
+  }
+
+  getShowMessageButton() {
+    return cy.get('#get-input > .btn').contains('Show Message')
+  }
+
+  getYourMessageText() {
+    return cy.xpath("//span[@id='display']")
+  }
+
+  getInput1Sum() {
+    return cy.get('#sum1')
+  }
+
+  getInput2Sum() {
+    return cy.get('#sum2')
+  }
+
+  getTotalButton() {
+    return cy.get('#gettotal > .btn').contains('Get Total')
+  }
+
+  getTotalResult() {
+    return cy.xpath("//span[@id='displayvalue']")
+  }
+}
+
+export default SimpleFormDemoElements
--- cypress/support/pageobjects/inputforms/SimpleFormDemoPage.ts
+++ cypress/support/pageobjects/inputforms/SimpleFormDemoPage.ts
@@ -1,4 +1,12 @@
+import SimpleFormDemoElements from '../../elements/inputforms_el/SimpleFormDemoElements'
+
 class SimpleFormDemoPage {
+  simpleFormDemo: any
+
+  constructor() {
+    this.simpleFormDemo = new SimpleFormDemoElements()
+  }
+
   /**
    * Method to check if the SimpleFormDemoPage is visible
    */
@@ -10,6 +18,34 @@ class SimpleFormDemoPage {
     cy.get('h3')
       .contains('This would be your first example to start with Selenium.')
       .should('be.visible')
+  }
+
+  enterMessage(message: string) {
+    this.simpleFormDemo.getUserMessageInput().click().clear().type(message)
+  }
+
+  clickOnShowMessage() {
+    this.simpleFormDemo.getShowMessageButton().click()
+  }
+
+  checkYourMessageIsCorrect(message: string) {
+    this.simpleFormDemo.getYourMessageText().should('have.text', message)
+  }
+
+  enterValueA(a: number) {
+    this.simpleFormDemo.getInput1Sum().click().clear().type(a)
+  }
+
+  enterValueB(b: number) {
+    this.simpleFormDemo.getInput2Sum().click().clear().type(b)
+  }
+
+  clickOnGetTotalutton() {
+    this.simpleFormDemo.getTotalButton().click()
+  }
+
+  checkTotalResultIsCorrect(total: number) {
+    this.simpleFormDemo.getTotalResult().should('have.text', total)
   }
 }
 
