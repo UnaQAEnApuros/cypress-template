8be4d173692e2d52ed6914d8ad872319b94db370
Estefanía Fdez Muñoz <estefafdez@gmail.com>
2024-07-01 09:39:18 +0200

    remove mocha

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -9,20 +9,6 @@ Cypress.on('fail', (error, runnable) => {
   onFailHandler(error, runnable);
 });
 
-const addContext = require('mochawesome/addContext'); // add failed screenshot to mochawesome report
-
-Cypress.on('test:after:run', (test, runnable) => {
-  // TODO: This is a mochawesome problem, that's why we skip those errors.
-  if (test.state === 'failed') {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore: Object is possibly 'undefined'.
-    const screenshotFileName = `${runnable.parent.title} -- ${test.title} (failed).png`;
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore: Type 'ObjectLike' is not assignable to type 'Runnable'.
-    addContext({ test }, `screenshots/${Cypress.spec.name}/${screenshotFileName}`);
-  }
-});
-
 /**
  * @description Method to avoid a failure on the tests due to uncaught exceptions.
  * More info here: https://docs.cypress.io/guides/references/error-messages.html#Uncaught-exceptions-from-your-application
